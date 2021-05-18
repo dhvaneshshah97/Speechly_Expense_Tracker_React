e2eca0c701b504fe6f4b71d7fbc03769fd496369
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2021-05-17 19:13:27 -0700

    Add List Component

---
+++ src/components/Main/List/List.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { List as MUIList, ListItem, ListItemAvatar, ListItemText, Avatar, ListItemSecondaryAction, IconButton, Slide } from '@material-ui/core';
+import { Delete, MoneyOff } from '@material-ui/icons';
+
+import useStyles from './styles';
+
+
+const List = () => {
+    const classes = useStyles();
+
+    const transactions = [
+        { id: 1, type: 'Income', category: 'Salary', amount: 50, date: 'Mon May 17 2021' }
+    ];
+
+    return (
+        <MUIList dense={false} className={classes.list}>
+            {transactions.map((transaction) => (
+                <Slide direction="down" in mountOnEnter unmountOnExit key={transaction.id}>
+                    <ListItem>
+                        <ListItemAvatar>
+                            <Avatar className={transaction.type === 'Income' ? classes.avatarIncome : classes.avatarExpense}>
+                                <MoneyOff />
+                            </Avatar>
+                        </ListItemAvatar>
+                        <ListItemText primary={transaction.category} secondary={`$${transaction.amount} - ${transaction.date}`} />
+                        <ListItemSecondaryAction>
+                            <IconButton edge="end" aria-label="delete" onCLick="">
+                                <Delete />
+                            </IconButton>
+                        </ListItemSecondaryAction>
+                    </ListItem>
+                </Slide>
+            ))}
+        </MUIList>
+    );
+}
+
+export default List;
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,17 +2,18 @@ import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 import useStyles from './styles';
 import Form from './Form/Form';
+import List from './List/List';
 
 const Main = () => {
     const classes = useStyles();
     return (
         <Card className={classes.root}>
-            <CardHeader title="Expense Tracker" subheader="Voice Controlled by Speechly" />
+            <CardHeader title="Expense Tracker" subheader="Voice Powered by Speechly" />
             <CardContent>
                 <Typography align="center" variant="h5">Total Balance $100</Typography>
                 <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
-                    {/*  */}
-                    Try saying: Add income
+                    {/* InfoCard */}
+                    Try saying: Add income for $100 in Category Salary for Monday ...
                 </Typography>
                 <Divider />
                 <Form />
@@ -20,7 +21,7 @@ const Main = () => {
             <CardContent className={classes.cartContent} >
                 <Grid container spacing={2}>
                     <Grid item xs={12}>
-                        list
+                        <List />
                     </Grid>
                 </Grid>
             </CardContent>
