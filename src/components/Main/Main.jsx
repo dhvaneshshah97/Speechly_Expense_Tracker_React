20765667f8d74672e75719cc75183abbeb262713
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2021-05-17 18:53:14 -0700

    Add Form in Main Component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 import useStyles from './styles';
+import Form from './Form/Form';
 
 const Main = () => {
     const classes = useStyles();
     return (
         <Card className={classes.root}>
-            <CardHeader title="Expense Tracker" subheader="Powered by Speechly" />
+            <CardHeader title="Expense Tracker" subheader="Voice Controlled by Speechly" />
             <CardContent>
                 <Typography align="center" variant="h5">Total Balance $100</Typography>
                 <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
@@ -14,7 +15,7 @@ const Main = () => {
                     Try saying: Add income
                 </Typography>
                 <Divider />
-                form
+                <Form />
             </CardContent>
             <CardContent className={classes.cartContent} >
                 <Grid container spacing={2}>
