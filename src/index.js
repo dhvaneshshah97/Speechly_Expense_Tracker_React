c1d6d2346d0606011222389cb2fdffb4b51cdeb3
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2021-06-05 14:19:46 -0700

    Integrate React Context

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import ReactDOM from 'react-dom';
 // main component
 import App from './App';
 import './index.css';
+import { Provider } from './context/context';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider>
+        <App />
+    </Provider>, document.getElementById('root')
+);
