c82538d96582b09c855b3a265039c3467a6648ba
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2021-06-09 11:34:50 -0700

    Add Infocard

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,6 +3,7 @@ import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@mater
 import useStyles from './styles';
 import Form from './Form/Form';
 import List from './List/List';
+import InfoCard from '../InfoCard';
 
 import { ExpenseTrackerContext } from '../../context/context';
 
@@ -16,8 +17,7 @@ const Main = () => {
             <CardContent>
                 <Typography align="center" variant="h5">Total Balance ${balance}</Typography>
                 <Typography variant="subtitle1" style={{ lineHeight: '1.5em', marginTop: '20px' }}>
-                    {/* InfoCard */}
-                    Try saying: Add income for $100 in Category Salary for Monday ...
+                    <InfoCard />
                 </Typography>
                 <Divider />
                 <Form />
