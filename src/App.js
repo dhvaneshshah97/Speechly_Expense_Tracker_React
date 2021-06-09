e174abe48c32d26599b47be2ca37b01c59f3d29e
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2021-06-09 12:20:29 -0700

    Mobile Responsive

--- src/App.js
+++ src/App.js
@@ -9,14 +9,17 @@ const App = () => {
     const classes = useStyles();
     return (
         <div>
-            <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '85vh', marginBottom:'50px' }} >
-                <Grid item xs={12} sm={3} >
+            <Grid className={classes.grid} container spacing={0} alignItems="center" justify="center" style={{ height: '85vh', marginBottom: '50px' }} >
+                <Grid item xs={12} sm={3} className={classes.mobile} >
                     <Details title="Income" />
                 </Grid>
-                <Grid item xs={12} sm={4}>
+                <Grid item xs={12} sm={4} className={classes.main}>
                     <Main />
                 </Grid>
-                <Grid item xs={12} sm={3}>
+                <Grid item xs={12} sm={3} className={classes.desktop}>
+                    <Details title="Income" />
+                </Grid>
+                <Grid item xs={12} sm={3} className={classes.last}>
                     <Details title="Expense" />
                 </Grid>
             </Grid>
